eef1d117a511d7ed3cde1de16990969381dcddb2
lostfields <nicolai.gjaerum@gmail.com>
2019-08-26 19:58:51 +0200

    updating version resolves #15

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pcomfortcloud',
-    version='0.0.13',
+    version='0.0.14',
     description='Read and change status of Panasonic Comfort Cloud devices',
     long_description=long_description,
     long_description_content_type="text/markdown",
