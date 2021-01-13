eeb216ce1f5fd447c98ccd45271ba31ab4e008ea
lostfields <nicolai.gjaerum@gmail.com>
2021-01-13 08:53:34 +0100

    bump version to correct version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pcomfortcloud',
-    version='0.0.18',
+    version='0.0.19',
     description='Read and change status of Panasonic Comfort Cloud devices',
     long_description=long_description,
     long_description_content_type="text/markdown",
